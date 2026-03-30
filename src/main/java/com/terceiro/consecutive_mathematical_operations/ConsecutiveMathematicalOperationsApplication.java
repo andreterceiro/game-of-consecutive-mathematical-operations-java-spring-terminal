34ba1cbcf9b28cce4688ceefab0abe03fa2efc04
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-03-30 09:12:00 -0300

    feat: initial files generated with Spring Initializr

---
+++ src/main/java/com/terceiro/consecutive_mathematical_operations/ConsecutiveMathematicalOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.terceiro.consecutive_mathematical_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsecutiveMathematicalOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsecutiveMathematicalOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terceiro/consecutive_mathematical_operations/ConsecutiveMathematicalOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terceiro.consecutive_mathematical_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsecutiveMathematicalOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
