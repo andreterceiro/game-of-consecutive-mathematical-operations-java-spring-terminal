1aa3e20cac6e5ac7541eb136f13aad5898761cea
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-04-10 09:25:47 -0300

    feat: doing the output for infinite numbers (tested for 3)

--- src/main/java/com/terceiro/consecutive_mathematical_operations/Main.java
+++ src/main/java/com/terceiro/consecutive_mathematical_operations/Main.java
@@ -1,12 +1,31 @@
 package com.terceiro.consecutive_mathematical_operations;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import java.util.Vector;
+import java.util.Random;
 
 @Component
 public class Main implements CommandLineRunner {
+    Vector<Integer> numbers = new Vector<>();
+
     @Override
     public void run(String ...args) throws Exception
     {
-        System.out.println("oi");
+        Random gerador = new Random();
+        numbers.add(gerador.nextInt(100));
+        numbers.add(gerador.nextInt(100));
+        numbers.add(gerador.nextInt(100));        
+        String output = "";
+
+        for (int number: numbers) {
+            output += number + " + ";
+        }
+        StringBuilder sb = new StringBuilder(output);
+
+        System.out.println(output.length());
+
+        sb.replace(output.length() - 2, output.length() -1, "=");
+
+        System.out.println(sb.toString());
     }
 }
