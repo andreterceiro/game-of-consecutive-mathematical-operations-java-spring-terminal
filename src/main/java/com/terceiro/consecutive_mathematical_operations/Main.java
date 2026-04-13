b27edfa4cec871b575084b8392372a533d0a50f1
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-04-13 09:18:12 -0300

    feat: asking the user to inform the sum

--- src/main/java/com/terceiro/consecutive_mathematical_operations/Main.java
+++ src/main/java/com/terceiro/consecutive_mathematical_operations/Main.java
@@ -3,6 +3,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.util.Vector;
 import java.util.Random;
+import java.util.Scanner;
 
 @Component
 public class Main implements CommandLineRunner {
@@ -12,6 +13,8 @@ public class Main implements CommandLineRunner {
     public void run(String ...args) throws Exception
     {
         Random gerador = new Random();
+        Scanner scanner = new Scanner(System.in);
+
         numbers.add(gerador.nextInt(100));
         numbers.add(gerador.nextInt(100));
         numbers.add(gerador.nextInt(100));        
@@ -21,10 +24,11 @@ public class Main implements CommandLineRunner {
             output += number + " + ";
         }
         StringBuilder sb = new StringBuilder(output);
-
         sb.replace(output.length() - 2, output.length() -1, "=");
-
-        System.out.println(numbers.stream().reduce(0,Integer::sum));
+        //System.out.println(numbers.stream().reduce(0,Integer::sum));
         System.out.println(sb.toString());
+        System.out.print("Digite a soma por favor: ");
+        Integer userSum = Integer.parseInt(scanner.nextLine());
+        System.out.println(userSum);
     }
 }
